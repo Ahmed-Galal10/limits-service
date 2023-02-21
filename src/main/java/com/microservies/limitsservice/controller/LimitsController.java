9bcf583befa715cbd47be4dc95cdca0f7e7c9454
Ahmed Galal <AhmedGalal10895@Gmail.com>
2023-02-21 15:01:24 +0200

    Create getLimits endpoint

--- src/main/java/com/microservies/limitsservice/controller/LimitsController.java
+++ src/main/java/com/microservies/limitsservice/controller/LimitsController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LimitsController {
 
     @GetMapping
-    public Limits getLimits(){
+    public Limits getLimits() {
         return new Limits(1, 2000);
     }
 }
